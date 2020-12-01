c851e3d25c494e5bd2a03d5b92d14f3ef5406ebb
Yemi Shin <shiny@carleton.edu>
2020-12-01 12:46:45 -0600

    fixing refreshing issues

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -86,3 +86,20 @@ $(function () {
 $("#name").focus(function () {
     $("#success").html("");
 });
+
+
+function successfulSend(){
+    // Success message
+    $("#success").html("<div class='alert alert-success'>");
+    $("#success > .alert-success")
+        .html(
+            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+        )
+        .append("</button>");
+    $("#success > .alert-success").append(
+        "<strong>Your message has been sent. </strong>"
+    );
+    $("#success > .alert-success").append("</div>");
+    //clear all fields
+    $("#contactForm").trigger("reset");
+}
